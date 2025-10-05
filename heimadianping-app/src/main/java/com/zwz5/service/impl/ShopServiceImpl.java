ae7005c547caf61ce91e45f3f043d34fbe55c54c
zhaowenzhuo <1214363136@qq.com>
2025-10-05 16:45:49 +0800

    解决缓存穿透

--- heimadianping-app/src/main/java/com/zwz5/service/impl/ShopServiceImpl.java
+++ heimadianping-app/src/main/java/com/zwz5/service/impl/ShopServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 import static com.zwz5.constants.RedisConstants.*;
@@ -34,17 +35,25 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     @Override
     public Shop queryById(Long id) {
+        // TODO Redisson 的 RBloomFilter 来实现布隆过滤器，后续补充
+
         // 查询缓存
         String key = CACHE_SHOP_KEY + id;
         String shopJson = stringRedisTemplate.opsForValue().get(key);
-        // 命中缓存，反序列化为对象返回
-        if (StringUtils.hasLength(shopJson)) {
+        // 命中有效缓存，反序列化为对象返回
+        if (StringUtils.hasText(shopJson)) {
            return jsonUtils.jsonToBean(shopJson, Shop.class);
+        }
+        // 命中空值缓存，则Result.fail
+        if (shopJson!=null && "".equals(shopJson)) {
+            throw new NullException("未查询到店铺");
         }
         // 未命中则查询数据库
         Shop shop = getById(id);
         if (shop == null) {
-            throw new NullException("未查询到商品");
+            // throw new NullException("未查询到店铺");
+            // 解决缓存穿透：将空值写入redis
+            stringRedisTemplate.opsForValue().set(key, "", CACHE_NULL_TTL, TimeUnit.MINUTES);
         }
         // 写入缓存
         stringRedisTemplate.opsForValue().set(key, jsonUtils.beanToJson(shop), CACHE_SHOP_TTL, TimeUnit.MINUTES);
@@ -63,5 +72,10 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         // 删除对应缓存
         String key = CACHE_SHOP_KEY + shop.getId();
         stringRedisTemplate.delete(key);
+        // 延迟双删，避免并发环境旧值回填
+        CompletableFuture.runAsync(() -> {
+            try { Thread.sleep(300); } catch (InterruptedException ignored) {}
+            stringRedisTemplate.delete(key);
+        });
     }
 }
